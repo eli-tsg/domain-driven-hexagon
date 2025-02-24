60b616f6750a9c625bc6408512be1fd8f23a388e
Ilya <ilya2005shkol@gmail.com>
2025-02-24 11:38:08 +0300

    Третье редактирование файла main.ts на мастер-ветке

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-// Комментарий для мастер-ветки (2)
+// Комментарий для мастер-ветки (3)
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
